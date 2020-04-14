e090dc50beb6d009d02d279e4933657f1f15942a
Tudums <soyka1998@gmail.com>
2020-04-14 10:15:05 +0300

    Начало

---
+++ unit1.pas
@@ -0,0 +1,113 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
+  Buttons, LCLType, ActnList, Clipbrd, Menus, DefaultTranslator, LCLTranslator,
+  EditBtn;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    CopyAction: TAction;
+    PasteAction: TAction;
+    ActionList1: TActionList;
+    Button1: TButton;
+    Button10: TButton;
+    Button11: TButton;
+    Button12: TButton;
+    Button13: TButton;
+    Button14: TButton;
+    Button15: TButton;
+    Button16: TButton;
+    Button17: TButton;
+    Button18: TButton;
+    Button19: TButton;
+    Button2: TButton;
+    Button20: TButton;
+    Button21: TButton;
+    Button22: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Button6: TButton;
+    Button7: TButton;
+    Button8: TButton;
+    Button9: TButton;
+    Edit1: TEdit;
+    MainMenu1: TMainMenu;
+    Edit: TMenuItem;
+    Copy1: TMenuItem;
+    Languages: TMenuItem;
+    Help: TMenuItem;
+    About: TMenuItem;
+    Help1: TMenuItem;
+    MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
+    MenuItem4: TMenuItem;
+    SpeedButton1: TSpeedButton;
+    Themes: TMenuItem;
+    Russian: TMenuItem;
+    English: TMenuItem;
+    Standart: TMenuItem;
+    Extended1: TMenuItem;
+    Mode: TMenuItem;
+    Paste: TMenuItem;
+    procedure CopyActionExecute(Sender: TObject);
+    procedure Button13Click(Sender: TObject);
+    procedure Button19Click(Sender: TObject);
+    procedure Button21Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Extended1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Extended1Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Button13Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.CopyActionExecute(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Button19Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Button21Click(Sender: TObject);
+begin
+
+end;
+
+end.
+
---
+++ unit2.pas
@@ -0,0 +1,107 @@
+unit Unit2;
+
+{$mode objfpc}{$H+}
+
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Buttons,
+  Menus, ActnList;
+
+type
+
+  { TForm2 }
+
+  TForm2 = class(TForm)
+    About: TMenuItem;
+    ActionList1: TActionList;
+    Button1: TButton;
+    Button10: TButton;
+    Button11: TButton;
+    Button12: TButton;
+    Button13: TButton;
+    Button14: TButton;
+    Button15: TButton;
+    Button16: TButton;
+    Button17: TButton;
+    Button18: TButton;
+    Button19: TButton;
+    Button2: TButton;
+    Button20: TButton;
+    Button21: TButton;
+    Button22: TButton;
+    Button23: TButton;
+    Button24: TButton;
+    Button25: TButton;
+    Button26: TButton;
+    Button27: TButton;
+    Button28: TButton;
+    Button29: TButton;
+    Button3: TButton;
+    Button30: TButton;
+    Button31: TButton;
+    Button32: TButton;
+    Button33: TButton;
+    Button34: TButton;
+    Button35: TButton;
+    Button36: TButton;
+    Button37: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Button6: TButton;
+    Button7: TButton;
+    Button8: TButton;
+    Button9: TButton;
+    Copy1: TMenuItem;
+    CopyAction: TAction;
+    Edit: TMenuItem;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    English: TMenuItem;
+    Extended1: TMenuItem;
+    Help: TMenuItem;
+    Help1: TMenuItem;
+    Label1: TLabel;
+    Languages: TMenuItem;
+    MainMenu1: TMainMenu;
+    MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
+    MenuItem4: TMenuItem;
+    Mode: TMenuItem;
+    Paste: TMenuItem;
+    PasteAction: TAction;
+    Russian: TMenuItem;
+    SpeedButton1: TSpeedButton;
+    SpeedButton2: TSpeedButton;
+    Standart: TMenuItem;
+    Themes: TMenuItem;
+    procedure Button28Click(Sender: TObject);
+    procedure Label1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm2 }
+
+procedure TForm2.Button28Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm2.Label1Click(Sender: TObject);
+begin
+
+end;
+
+end.
+
